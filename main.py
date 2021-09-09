1b0fb34804be05e483a1358eb424e28ff5ad503e
Aleksandr <menshikov.a.v@icloud.com>
2021-09-09 10:19:10 +0300

    Add a try-except block

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 from pathlib import Path
@@ -5,6 +6,7 @@ from random import randint
 
 import comics_processing
 from dotenv import load_dotenv
+from requests.exceptions import ConnectionError, HTTPError, InvalidURL
 from vk_post import publish_comics
 
 
@@ -16,16 +18,24 @@ def main():
 
     Path('images').mkdir(exist_ok=True)
 
-    comics_quantity = comics_processing.get_comics_quantity()
-    comics_number = randint(1, comics_quantity)
+    try:
+        comics_quantity = comics_processing.get_comics_quantity()
+        comics_number = randint(1, comics_quantity)
 
-    comics = comics_processing.download_comics(comics_number)
-    comics_name = comics_processing.get_img_name(comics['img'])
-    comics_comment = comics['alt']
-    comics_processing.get_img(comics['img'], comics_name)
+        comics = comics_processing.download_comics(comics_number)
+        comics_name = comics_processing.get_img_name(comics['img'])
+        comics_comment = comics['alt']
 
-    publish_comics(comics_name, comics_comment, group_id,
-                   access_token, API_version)
+        comics_processing.get_img(comics['img'], comics_name)
+    except (ConnectionError, InvalidURL, HTTPError) as error:
+        logging.error(f"{error}\nCan't get data from xkcd.com.")
+    
+    try:
+        publish_comics(comics_name, comics_comment, group_id,
+                       access_token, API_version)
+    except (ConnectionError, InvalidURL, HTTPError) as error:
+        logging.error(f"{error}Can't get data from api.vk.com")
+
     shutil.rmtree('images')
 
 
