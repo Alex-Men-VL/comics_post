cfb08aa5e2260a6f6edbf00a369aff8bfb2ab046
Aleksandr <menshikov.a.v@icloud.com>
2021-09-12 14:57:31 +0300

    Reduce the try except blocks

--- main.py
+++ main.py
@@ -16,19 +16,23 @@ def main():
 
     try:
         comics_quantity = comics_processing.get_comics_quantity()
-        comics_number = randint(1, comics_quantity)
     except (ConnectionError, InvalidURL, HTTPError) as err:
         logging.error(f"{err}\nCan't get data from "
                       "https://xkcd.com/info.0.json.")
+        return
+
+    comics_number = randint(1, comics_quantity)
 
     try:
         comics = comics_processing.get_comics(comics_number)
-        comics_name = comics_processing.get_image_name(comics['img'])
-        comics_comment = comics['alt']
-        image_url = comics['img']
     except (ConnectionError, InvalidURL, HTTPError) as err:
         logging.error(f"{err}\nCan't get data from "
                       f"https://xkcd.com/{comics_number}/info.0.json.")
+        return
+
+    comics_name = comics_processing.get_image_name(comics['img'])
+    comics_comment = comics['alt']
+    image_url = comics['img']
 
     try:
         comics_processing.download_image(image_url, comics_name)
